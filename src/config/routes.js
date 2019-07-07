65ecb89d004ae792916ff394e86245d5bf459c42
bj-budhathoki <bijayabudhathoki98@gmail.com>
2019-07-07 20:45:49 +0545

    form

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
 import ShopPage from "../pages/ShopPage/ShopPage";
+import SignInAndSignUp from "../pages/SignInAndSignUp/SignInAndSignUp";
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/shop" component={ShopPage} />
+      <Route exact path="/signin" component={SignInAndSignUp} />
     </Switch>
   );
 };
---
+++ src/pages/SignInAndSignUp/SignInAndSignUp.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Signin from "../../components/SignIn/SignIn";
+class SignInAndSignUp extends Component {
+  render() {
+    return (
+      <section className="sign-in-and-sign-up">
+        <Signin />
+      </section>
+    );
+  }
+}
+export default SignInAndSignUp;
