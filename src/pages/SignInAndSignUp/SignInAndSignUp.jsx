e47a7e90599e398246e5e1064c6a2a0ad1a8a522
bj-budhathoki <bijayabudhathoki98@gmail.com>
2019-07-08 23:50:52 +0545

    signup

--- src/pages/SignInAndSignUp/SignInAndSignUp.jsx
+++ src/pages/SignInAndSignUp/SignInAndSignUp.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Signin from "../../components/SignIn/SignIn";
+import SignUp from "../../components/Signup/SignUp";
 class SignInAndSignUp extends Component {
   render() {
     return (
       <section className="sign-in-and-sign-up">
         <Signin />
+        <SignUp />
       </section>
     );
   }
